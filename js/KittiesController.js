6c14a3b0cdbfae7cc53e9813f8b63d4003771597
YOUR-USERNAME <YOUR-EMAIL-ADDRESS>
2014-12-05 19:24:10 -0800

    update all code for working version

--- js/KittiesController.js
+++ js/KittiesController.js
@@ -1,31 +1,54 @@
-[
-		{
-			catName: 'Karl',
-			monthsOld: 5,
-			gender: 'boy',
-			image: "images/carl.png",
-			adopted: true,
-		},
-		{
-			catName: 'Jack',
-			monthsOld: 4,
-			gender: 'boy',
-			image: "images/jack.png",
-			adopted: false
-		},
-		{
-			catName: 'Oscar',
-			monthsOld: 2,
-			gender: 'boy',
-			image: "images/oscar.png",
-			adopted: false
-		},
-		{
-			catName: 'Princess Mew',
-			monthsOld: 3,
-			gender: 'girl',
-			image: "images/princessmew.png",
-			adopted: false
+angular
+	.module("kittiesApp")
+	.controller("KittiesController", KittiesController);
+
+
+	
+
+	function KittiesController (){
+			this.kittieArray = [
+					{
+						catName: 'Karl',
+						monthsOld: 5,
+						gender: 'boy',
+						image: "images/carl.png",
+						adopted: true,
+					},
+					{
+						catName: 'Jack',
+						monthsOld: 4,
+						gender: 'boy',
+						image: "images/jack.png",
+						adopted: false
+					},
+					{
+						catName: 'Oscar',
+						monthsOld: 2,
+						gender: 'boy',
+						image: "images/oscar.png",
+						adopted: false
+					},
+					{
+						catName: 'Princess Mew',
+						monthsOld: 3,
+						gender: 'girl',
+						image: "images/princessmew.png",
+						adopted: false
+					}
+
+				]
+
+		this.adopted = adopted;
+		this.unadopted=unadopted;
+
+		function adopted(kitten){
+			kitten.adopted=true;
 		}
 
-	]
+		function unadopted(kitten){
+			kitten.adopted=false;
+		}
+
+
+	}
+
